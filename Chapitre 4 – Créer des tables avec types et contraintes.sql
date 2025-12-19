19ff5c1b1c99a55b286aa1e8bc5fa6b5e2e83262
Pikichtaine <melihizakaria58@gmail.com>
2025-12-19 09:31:58 +0000

    MySQL

--- Chapitre 4 – Créer des tables avec types et contraintes.sql
+++ Chapitre 4 – Créer des tables avec types et contraintes.sql
@@ -1,11 +1,23 @@
+
+--Créer la base de données
+
 create database Mon_Blog;
+
+--Utiliser la base de données
+
 use Mon_Blog;
+
+--Créer la table Utilisateur
+
 create table Utilisateur (
 id int auto_increment primary key,
 nom varchar(100) not null,
 email varchar(100) unique,
 mot_de_passe varchar(255) not null
 );
+
+--Créer la table Article
+
 create table Article (
 id int auto_increment primary key,
 titre varchar(150) not null,
@@ -14,6 +26,9 @@ date_pub date,
 id_utilisateur int,
 foreign key(id_utilisateur) references utilisateur(id)
 );
+
+--Créer la table Commentaire
+
 create table Commentaire (
 id int auto_increment primary key,
 contenu text not null,
@@ -22,6 +37,10 @@ date_commentaire date,
 id_article int,
 foreign key(id_article) references article(id)
 );
+
+
+
+
 
 
 
