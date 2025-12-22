3359109da956df8257907a64e8c0fd01c422ee83
Pikichtaine <melihizakaria58@gmail.com>
2025-12-22 12:09:19 +0100

    MySQL

---
+++ Réalisation.sql
@@ -0,0 +1,51 @@
+
+--Créer la base de données
+
+create database Mon_Blog;
+
+--Utiliser la base de données
+
+use Mon_Blog;
+
+--Créer la table Utilisateur
+
+create table Utilisateur (
+id int auto_increment primary key,
+nom varchar(100) not null,
+email varchar(100) unique,
+mot_de_passe varchar(255) not null
+);
+
+--Créer la table Article
+
+create table Article (
+id int auto_increment primary key,
+titre varchar(150) not null,
+contenu text not null,
+date_pub date,
+id_utilisateur int,
+foreign key(id_utilisateur) references utilisateur(id)
+);
+
+--Créer la table Commentaire
+
+create table Commentaire (
+id int auto_increment primary key,
+contenu text not null,
+auteur varchar(100),
+date_commentaire date,
+id_article int,
+foreign key(id_article) references article(id)
+);
+
+--(Tester l’insertion)
+
+insert into Utilisateur(nom, email, mot_de_passe)values('Zakaria', 'melihizakaria58@gmail.com', 'LoleritoDankito');
+
+--Ajouter un Index
+
+alter table utilisateur add index (email);
+
+--Ajouter un commentaire
+
+alter table utilisateur comment='crée la table utilisateur';
